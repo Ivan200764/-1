8a61dfec6a0be624e37d4f073181eed1dd8577ac
Ivan200764 <101586300+Ivan200764@users.noreply.github.com>
2022-03-15 18:53:29 +0400

    Colculator
    Colculator

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstvalue, twovalue;
+            string operatorA;
+
+            Console.WriteLine("Введи первое число: ");
+            firstvalue = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число ");
+            twovalue = Double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Что делаем?: + | - | / | * |");
+            operatorA = Console.ReadLine();
+
+            switch (operatorA)
+            {
+                case "+":
+                    Console.WriteLine(firstvalue + twovalue);
+                    break;
+
+                case "-":
+                    Console.WriteLine(firstvalue - twovalue);   
+                    break;
+                
+                case "*":
+                    Console.WriteLine(firstvalue * twovalue);
+                    break;
+
+                case "/":
+                    if (firstvalue == 0)
+                        Console.WriteLine(0);
+                    else if (twovalue == 0)
+                        Console.WriteLine(0);
+                    else
+                        Console.WriteLine(firstvalue / twovalue);
+                    break;
+
+                    default:
+                    Console.WriteLine("Неизвестно");
+                        break;
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
